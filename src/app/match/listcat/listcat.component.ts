d08d8d3b7006aaa8867c5dc11c738f9b642d83e9
chareef <howlong53@gmail.com>
2017-09-06 16:01:51 +0700

    two

--- src/app/match/listcat/listcat.component.ts
+++ src/app/match/listcat/listcat.component.ts
@@ -10,13 +10,14 @@ import * as myGlobal from '../../global'
   providers:[HttpService]
 })
 export class ListcatComponent implements OnInit {
-
+  hostphp =myGlobal.hostphp
   	constructor(private fetchdata:HttpService) { }
   	random = new Date().getTime()+new Date().getMilliseconds()
 	getCatdata : string
   	ngOnInit() {
 	  	this.fetchdata.get_json(myGlobal.hostphp+'/backend/addmatch/list_allcat?nocache='+this.random).subscribe(
 	  			data => {this.getCatdata = data
+            console.log(this.getCatdata);
 	  			}
 	  		)
  	}
